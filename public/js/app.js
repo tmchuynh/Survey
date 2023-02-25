1776a3cef5bc9b2ae415d00655cd769ac8a6fa74
Tina Huynh <tinamchuynh@gmail.com>
2023-02-25 14:33:48 -0800

    add character buttons

--- public/js/app.js
+++ public/js/app.js
@@ -2,6 +2,9 @@ var name_field = document.querySelector("#name_field");
 var nes_field = document.querySelector(".nes-field");
 var message_intro = document.querySelector(".message_intro");
 var message_body = document.querySelector(".message_body");
+var character_choice = document.querySelector(".character_choice");
+var buttons = document.querySelectorAll(".button-group button");
+var button_group = document.querySelector(".button-group");
 var intro = document.querySelector(".intro");
 var text_field = document.querySelector(".text_field");
 var textarea_field = document.querySelector("#textarea_field");
@@ -20,8 +23,20 @@ name_field.addEventListener("keyup", function (event) {
 function displayDialog() {
     nes_field.classList.add("d-none");
     message_intro.classList.remove("d-none");
-    text_field.classList.remove("d-none");
     intro.innerHTML = "Hello " + person + "!";
+    character_choice.classList.remove("d-none");
+    buttons.forEach(element => {
+        element.addEventListener("click", function (event) {
+            console.log(element.innerHTML);
+            text_field.classList.remove("d-none");
+    button_group.classList.add("d-none");
+
+        });
+    });
+
+}
+
+function displayBody() {
     textarea_field.addEventListener("keyup", function (event) {
         if (event.keyCode === 13) {
             body.innerHTML = textarea_field.value;
@@ -29,5 +44,4 @@ function displayDialog() {
             message_body.classList.remove("d-none");
         }
     });
-
 }
